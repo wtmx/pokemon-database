a7a49074e55e314628d9c0f11cce931e9a5e22c1
Glitch (glitch-hello-node) <none>
2024-04-14 14:56:52 +0000

    💸🚪 Checkpoint ./index.js:66600604/3426

--- index.js
+++ index.js
@@ -6,28 +6,33 @@ const notion = new Client({auth: process.env.NOTION_KEY})
 const pokeArray = []
 
 async function getPokemon() {
-  await axios.get('https://pokeapi.co/api/v2/pokemon/2')
-    .then((poke) => {
-      //console.log(poke)
-      const pokeData = {
-        "name": poke.data.species.name,
-        "number": poke.data.id,
-        "hp": poke.data.stats[0].base_stat,
-        "height": poke.data.height,
-        "weight": poke.data.weight,
-        "attack": poke.data.stats[1].base_stat,
-        "defense": poke.data.stats[2].base_stat,
-        "special-attack": poke.data.stats[3].base_stat,
-        "special-defense": poke.data.stats[4].base_stat,
-        "speed": poke.data.stats[5].base_stat,
-      }
-      
-      pokeArray.push(pokeData)
-      console.log(`Fetching ${pokeData.name} from PokeAPI.`)
-  })
-    .catch((error) => {
-      console.log(error)
-  })
+  
+  
+  for (let i = 1; i <= 10; i++) {
+      await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then((poke) => {
+        //console.log(poke)
+        const pokeData = {
+          "name": poke.data.species.name,
+          "number": poke.data.id,
+          "hp": poke.data.stats[0].base_stat,
+          "height": poke.data.height,
+          "weight": poke.data.weight,
+          "attack": poke.data.stats[1].base_stat,
+          "defense": poke.data.stats[2].base_stat,
+          "special-attack": poke.data.stats[3].base_stat,
+          "special-defense": poke.data.stats[4].base_stat,
+          "speed": poke.data.stats[5].base_stat,
+        }
+
+        pokeArray.push(pokeData)
+        console.log(`Fetching ${pokeData.name} from PokeAPI.`)
+    })
+      .catch((error) => {
+        console.log(error)
+    })
+  }
+
   
   createNotionPage()
 }
